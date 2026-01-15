e7d502124cd7cfa8f201bb6c379e1253e7dc1943
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2026-01-15 13:57:42 +0100

    Handle invalid command

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,7 @@ const rl = createInterface({
 });
 
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
+rl.question("$ ", (answer: string) => {
+  console.log(`${answer}: command not found`)
   rl.close();
 });

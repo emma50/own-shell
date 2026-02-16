eac48e21990ea7cf9268e366bac70024b424fcdf
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2026-02-16 08:29:43 +0100

    WIP:Refractor main.ts

--- app/main.ts
+++ app/main.ts
@@ -9,41 +9,41 @@ const rl = createInterface({
 function prompt() {
   const builtInCommands = ["echo", "exit", "type"];
   rl.question("$ ", (answer: string) => {
+    answer = answer.trim();
     // // Exit the terminal on "exit" command
     if (answer === "exit") {
       rl.close();
       return;
     }
 
-    const trimmed = answer.trim();
+    // const trimmed = answer.trim();
 
-    if (trimmed.length === 0) {
+    if (answer.length === 0) {
       prompt();
     }
 
-    const [command, ...args] = trimmed.split(" ");
+    const [command, ...args] = answer.split(" ");
 
     if (command === "type") {
-      const [first] = args
+      const [first] = args;
 
       if (builtInCommands.includes(first)) {
-        console.log(`${first} is a shell builtin`)
-      }
-      else {
-        console.log(`${first}: not found`)
+        console.log(`${first} is a shell builtin`);
+      } else {
+        console.log(`${first}: not found`);
       }
     } else if (command === "echo") {
       if (args.length === 0) {
-        console.log()
+        console.log();
       }
-      console.log(args.join(" "))
+      console.log(args.join(" "));
     } else {
-      console.log(`${command}: command not found`)
+      console.log(`${command}: command not found`);
     }
 
     // Repeat the prompt
-    prompt()
-  })
+    prompt();
+  });
 }
 
-prompt()
+prompt();

fe65d7c55e6dccf9ea06c28fc5758800b7545c44
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2026-02-18 16:08:10 +0100

    WIP: Fix Run a program error

--- app/main.ts
+++ app/main.ts
@@ -6,62 +6,7 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-// TODO: Uncomment the code below to pass the first stage
-function prompt() {
-  const builtInCommands = ["echo", "exit", "type"];
-  rl.question("$ ", (answer: string) => {
-    answer = answer.trim();
-    // // Exit the terminal on "exit" command
-    if (answer === "exit") {
-      rl.close();
-      return;
-    }
-
-    if (answer.length === 0) {
-      prompt();
-      return;
-    }
-
-    const [command, ...args] = answer.split(" ");
-
-    if (command === "type") {
-      const [first] = args;
-
-      if (!first) {
-        prompt();
-        return;
-      }
-
-      if (builtInCommands.includes(first)) {
-        console.log(`${first} is a shell builtin`);
-      } else {
-        try {
-          const result = findExecutables(first);
-          console.log(`${first} is ${JSON.stringify(result?.location)}`);
-        } catch {
-          console.log(`${first}: not found`);
-        }
-      }
-    } else if (command === findExecutables(command)?.command) {
-      // If the command is found in the system's PATH, it executes the command with the provided arguments.
-      try {
-        execSync(`${command} ${args.join(" ")}`, { stdio: "inherit" });
-      } catch (error) {
-        console.error(`Error executing command: ${error}`);
-      }
-    } else {
-      // ✅ Default case for unknown commands
-      // console.log(`${command}: command not found`);
-      console.log(
-        `Program was passed ${args.length + 1} args (including program name).`,
-      );
-    }
-    // Repeat the prompt
-    prompt();
-  });
-}
-
-const findExecutables = (executable: string) => {
+function findExecutable(executable: string) {
   // On Windows, the `where` command is used to find the location of executables, while on Unix-like systems, the `which` command serves the same purpose.
   const isWindows = process.platform === "win32";
 
@@ -81,13 +26,59 @@ const findExecutables = (executable: string) => {
   } catch {
     console.log(`${executable}: not found`);
   }
-};
+}
 
 function getFileName(filePath: string) {
   // Get the last part of the path (cat.exe)
   const baseName = filePath.split("\\").pop();
   // Remove the extension (.exe)
   return (baseName as string).replace(/\.[^/.]+$/, "");
+}
+
+function prompt() {
+  rl.question("$ ", (answer: string) => {
+    answer = answer.trim();
+
+    if (answer.length === 0) {
+      prompt();
+      return;
+    }
+    // // Exit the terminal on "exit" command
+    if (answer === "exit") {
+      rl.close();
+      return;
+    }
+
+    const [command, ...args] = answer.split(" ");
+
+    if (command === "type") {
+      const builtInCommands = ["echo", "exit", "type"];
+      const [first] = args;
+
+      if (!first) {
+        prompt();
+        return;
+      }
+
+      if (builtInCommands.includes(first)) {
+        console.log(`${first} is a shell builtin`);
+      } else {
+        try {
+          const result = findExecutable(first);
+          console.log(`${first} is here ${JSON.stringify(result?.location)}`);
+        } catch {
+          console.log(`${first}: not found`);
+        }
+      }
+    } else if (command === findExecutable(command)?.command) {
+      console.log(`${command} is here ${findExecutable(command)?.location}`);
+    } else {
+      // ✅ Default case for unknown commands
+      console.log(`${command}: command not found`);
+    }
+    // Repeat the prompt
+    prompt();
+  });
 }
 
 prompt();

cb7de07ed7451c66cb95181b3f29128ba4027216
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2026-02-27 23:37:05 +0100

    Fix typo in parameter name in runExternal function

--- app/main.ts
+++ app/main.ts
@@ -505,7 +505,7 @@ function runExternal(
   args: string[],
   redirection: Redirection | null,
 ) {
-  const child = spawn(command, args, { stdio: "pipe" });
+  const child = spawn(command, args, { stdio: ["inherit", "pipe", "pipe"] });
 
   if (redirection) {
     const stream = fs.createWriteStream(redirection.file, {
